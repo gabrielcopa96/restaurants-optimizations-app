3c19dca00e7fe9c7514393fea7fa3bad4a8f9345
Gabriel <gabrielcncopa@gmail.com>
2023-04-01 01:46:18 -0300

    add partial backoffice

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import SignUp from "./components/autentication/SignUp";
 import SignIn from "./components/autentication/SignIn";
+import Access from "./views/view-access/Access";
 const App = () => {
 
   return (
     <div >
+      <Access />
       {/* <SignUp/>
       <SignIn/> */}
     </div>
